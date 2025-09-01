c20b31f08dff4bedf53b1e947b31cc5f8842ba1a
Jonatas Benicio <jonathas_santos93@hotmail.com>
2025-09-01 18:29:51 -0300

    refactor: adiciona pacote correto ao enum

--- src/main/java/com/jb/controller/AlertsController.java
+++ src/main/java/com/jb/controller/AlertsController.java
@@ -16,9 +16,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import builders.JsonStructureBuilder;
-import utils.HTTPMethods;
+import enums.HTTPMethods;
 
 @RestController
 @CrossOrigin(origins = "*")
