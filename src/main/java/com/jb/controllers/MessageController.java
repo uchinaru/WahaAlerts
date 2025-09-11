fce45d00f60c3c7683663970ae5999c7e435ac61
Jonatas Benicio <jonathas_santos93@hotmail.com>
2025-09-11 19:29:32 -0300

    cleanup: Eliminando blocos de código não utilizado

--- src/main/java/com/jb/controllers/MessageController.java
+++ src/main/java/com/jb/controllers/MessageController.java
@@ -1,6 +1,5 @@
 package com.jb.controllers;
 
-import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,9 +7,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.jb.config.PropertiesConfig;
-import com.jb.repository.DespesasRepository;
-import com.jb.repository.UserRepository;
 import com.jb.services.ProcessMessageService;
 
 @RestController
@@ -20,9 +16,6 @@ public class MessageController {
 	
 	@Autowired
 	ProcessMessageService processMessageService;
-	
-	public static HttpPost post;
-	public String mensagemAnterior = "";
 	
 	@PostMapping("/message")
 	public void reciveMessages(@RequestBody String mensagem) {
