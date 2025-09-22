7afe7ca357b944e64a416305918f9ae2bf5439bd
Jonatas Benicio <jonathas_santos93@hotmail.com>
2025-09-21 21:54:06 -0300

    refactor: renomeia o import para DTO

--- src/main/java/com/jb/utils/JsonUtils.java
+++ src/main/java/com/jb/utils/JsonUtils.java
@@ -2,14 +2,14 @@ package com.jb.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jb.model.JsonWahaModel;
+import com.jb.model.DTOJsonWaha;
 
 public class JsonUtils {
 	
-	public static JsonWahaModel transformJsonInObjectWaha(String message) {
+	public static DTOJsonWaha transformJsonInObjectWaha(String message) {
 		try {
 			ObjectMapper bjectMapper = new ObjectMapper();
-			return bjectMapper.readValue(message, JsonWahaModel.class);
+			return bjectMapper.readValue(message, DTOJsonWaha.class);
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
 			return null;
